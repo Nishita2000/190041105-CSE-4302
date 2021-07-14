a979e1cffb01aef2b92af0a1fb04b226e9604967
Nishita2000 <84925367+Nishita2000@users.noreply.github.com>
2021-07-15 03:08:26 +0600

    Create 105_task_2.cpp

---
+++ Lab 06/105_task_2.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class FLOAT
+{
+private:
+    float data;
+
+public:
+    FLOAT() {}
+    FLOAT(float d) : data(d)
+    {
+    }
+    void setValue(float d)
+    {
+        data = d;
+    }
+    void Display(void)
+    {
+        cout << "Data: " << data << endl;
+    }
+    FLOAT operator+(FLOAT f)
+    {
+        return FLOAT(data + f.data);
+    }
+    FLOAT operator-(FLOAT f)
+    {
+        return FLOAT(data - f.data);
+    }
+    FLOAT operator*(FLOAT f)
+    {
+        return FLOAT(data * f.data);
+    }
+    FLOAT operator/(FLOAT f)
+    {
+        return FLOAT(data / f.data);
+    }
+    operator int() const
+    {
+        return int(data);
+    }
+};
+
+int main(void)
+{
+    FLOAT f1(5.7), f2(9.8), f3, f4;
+    f3.setValue(4.33);
+    f1.Display();
+    f2.Display();
+    f3.Display();
+    f4 = f1 + f2;
+    f4.Display();
+    f4 = f1 - f3;
+    f4.Display();
+    f4 = f1 * f2;
+    f4.Display();
+    f4 = f2 / f3;
+    f4.Display();
+    int i = f1;
+    cout << "Int: " << i << endl;
+}
